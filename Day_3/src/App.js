c2b9b58253531ecd04133645fdd7275b9cdd048e
MOHAMED HASHEEM N <114977778+MOHAMED-HASHEEM-N@users.noreply.github.com>
2023-07-11 16:02:24 +0530

    Add files via upload

---
+++ Day_3/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import Home from "./Components/Home";
+
+const App = () => {
+  
+  return (
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <Link to={"/"} className="navbar-brand">
+          Banking App
+        </Link>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/home"} className="nav-link">
+              Home
+            </Link>
+          </li>
+        </div>
+
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link to={"/register"} className="nav-link">
+                Sign Up
+              </Link>
+            </li>
+          </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
