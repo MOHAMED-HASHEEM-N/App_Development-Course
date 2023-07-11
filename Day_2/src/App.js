24af4eea7f6b9efd0a35f7eb55920ada5d18e2e6
MOHAMED HASHEEM N <114977778+MOHAMED-HASHEEM-N@users.noreply.github.com>
2023-07-11 12:26:21 +0530

    Add files via upload

---
+++ Day_2/src/App.js
@@ -0,0 +1,13 @@
+import RegistrationForm from './signup';
+import Login from './login';
+
+import Routing from './Router';
+function App() {
+  return (
+    <div >
+    <Routing></Routing>
+    </div>
+    );
+  }
+  
+export default App;
